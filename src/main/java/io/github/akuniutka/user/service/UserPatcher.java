17e410187575f53f82f59bbbd8ae25c7dfa90f8b
Andrei Kuniutka <akuniutka@gmail.com>
2025-05-15 23:48:00 +0300

    Docs: Add description of UserPatcher interface method
    Add a description of the only 'UserPatcher' interface method as
    guidance for implementations of that interface.

--- src/main/java/io/github/akuniutka/user/service/UserPatcher.java
+++ src/main/java/io/github/akuniutka/user/service/UserPatcher.java
@@ -4,5 +4,15 @@ import io.github.akuniutka.user.entity.User;
 
 public interface UserPatcher {
 
+    /**
+     * For each {@code user}'s property sets its value to that of the relative property in the {@code patch}
+     * when the value of the property in the {@code patch} is not null and differs from the current value of
+     * {@code user}'s property.
+     *
+     * @param patch the patch to be applied to the {@code user}
+     * @param user  the user to be updated
+     * @return {@code true} if any of {@code user}'s properties has changed in the result of update and {@code false}
+     * otherwise
+     */
     boolean applyPatchToUser(User patch, User user);
 }
